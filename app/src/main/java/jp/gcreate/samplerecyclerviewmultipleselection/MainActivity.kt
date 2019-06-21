8253c7b4769a3025dbec7461caf2d2e24f0e5f23
Gen <archiherewego@gmail.com>
2019-06-21 10:47:28 +0900

    Use DataBinding instead of kotlin-android-extension

--- app/src/main/java/jp/gcreate/samplerecyclerviewmultipleselection/MainActivity.kt
+++ app/src/main/java/jp/gcreate/samplerecyclerviewmultipleselection/MainActivity.kt
@@ -1,21 +1,23 @@
 package jp.gcreate.samplerecyclerviewmultipleselection
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+import jp.gcreate.samplerecyclerviewmultipleselection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     
+    private lateinit var binding: ActivityMainBinding
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
         
-        fab.setOnClickListener { view ->
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
